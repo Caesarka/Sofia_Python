35c45fe1591cc468fb7cc080b960b08896642dba
Julianna Gusarova <julianna.gusasora@gmail.com>
2025-10-09 14:35:15 -0400

    check api tests and realty controller

--- Flask_SQLite_practice/tests/api_tests.py
+++ Flask_SQLite_practice/tests/api_tests.py
@@ -10,7 +10,7 @@ class ServerTests(unittest.TestCase):
         self.url = os.getenv("TARGET_URL", "http://localhost:5000/")
         pass
 
-    def testPostRealty(self):
+    def test_post_get_realty(self):
         guid = uuid.uuid4()
         url = f"{self.url}/api/realty/"
         payload = {
@@ -34,13 +34,17 @@ class ServerTests(unittest.TestCase):
         found = any(payload["title"] == item.get("title") for item in data)
         self.assertTrue(found, f"Response does not contain item with title {payload['title']}")
 
-    def testGetRealty(self):
-        url = f"{self.url}/api/realty/?id=30"
-        response = requests.get(url)
-        data = response.json()
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(data["title"], "Townhouse")
-
+    #def test_delete_realty(self):
+    #    url = f"{self.url}/api/realty/1"
+    #    payload = {
+    #       "title": "My new realty",
+    #       "price": 1000000,
+    #       "city": "Toronto",
+    #       "address": "address",
+    #       "image": "image",
+    #    }
+    #    response = requests.post(url, json=payload)
+    #    response = requests.delete(url)
     
 
 if __name__ == '__main__':
