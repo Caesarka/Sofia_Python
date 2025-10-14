b0b74b7a8568b607cebad85dbebc56e0f619d35e
Julianna Gusarova <julianna.gusasora@gmail.com>
2025-10-14 15:09:24 -0400

    delete, put tests

--- Flask_SQLite_practice/tests/api_tests.py
+++ Flask_SQLite_practice/tests/api_tests.py
@@ -34,18 +34,52 @@ class ServerTests(unittest.TestCase):
         found = any(payload["title"] == item.get("title") for item in data)
         self.assertTrue(found, f"Response does not contain item with title {payload['title']}")
 
-    #def test_delete_realty(self):
-    #    url = f"{self.url}/api/realty/1"
-    #    payload = {
-    #       "title": "My new realty",
-    #       "price": 1000000,
-    #       "city": "Toronto",
-    #       "address": "address",
-    #       "image": "image",
-    #    }
-    #    response = requests.post(url, json=payload)
-    #    response = requests.delete(url)
+    def test_delete_realty(self):
+        url = f"{self.url}/api/realty/"
+        payload = {
+           "title": "My new realty",
+           "price": 1000000,
+           "city": "Toronto",
+           "address": "address",
+           "image": "image",
+        }
+        response = requests.post(url, json=payload)
+
+        realty_id = response.json()["id"]
+        print(realty_id)
+        response = requests.delete(f"{url}{realty_id}")
+        self.assertEqual(response.status_code, 200)
     
+    def test_update_realty(self):
+        url = f"{self.url}/api/realty/"
+        payload_post = {
+           "title": "My new realty",
+           "price": 1000000,
+           "city": "Toronto",
+           "address": "address",
+           "image": "image",
+        }
+        response = requests.post(url, json=payload_post)
+        self.assertEqual(response.status_code, 201)
+        realty_id = response.json().get("id")
+        self.assertIsNotNone(realty_id)
+
+        payload_update = {
+           "id": realty_id,
+           "title": "Updated title",
+           "price": 44355,
+           "city": "New city",
+           "address": "New address",
+           "image": "image",
+        }
+        response_update = requests.put(f"{url}{realty_id}", json=payload_update)
+        self.assertEqual(response_update.status_code, 200, f"{response_update.json()}")
+        check_response = requests.get(f"{url}{realty_id}").json()
+        print(f"Updated data: {check_response}")
+        self.assertEqual(check_response["title"], "Updated title")
+        self.assertEqual(check_response["price"], 44355)
+
+
 
 if __name__ == '__main__':
     unittest.main()
