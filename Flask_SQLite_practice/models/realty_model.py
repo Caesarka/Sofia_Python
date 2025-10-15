e4f93b72344ba2d2dcc1861cc97e465aa69281f7
Julianna Gusarova <julianna.gusasora@gmail.com>
2025-10-15 16:12:44 -0400

    user db methods,  db unit tests

--- Flask_SQLite_practice/models/realty_model.py
+++ Flask_SQLite_practice/models/realty_model.py
@@ -11,23 +11,3 @@ class Realty(BaseModel):
 
     class Config:
         orm_mode = True
-#        print(dictData)
-#        print(type(dictData))
-#
-#        self.id = dictData.get("id", None)
-#        self.title = dictData.get("title", None)
-#        self.price = dictData.get("price", None)
-#        self.city = dictData.get("city", None)
-#        self.address = dictData.get("address", None)
-#        self.image = dictData.get("image", None)
-#
-#    def to_dict(self):
-#        data = {
-#            "id": self.id,
-#            "title": self.title,
-#            "price": self.price,
-#            "city": self.city,
-#            "address": self.address,
-#            "image": self.image
-#        }
-#        return data
--- Flask_SQLite_practice/tests/unit_database_tests.py
+++ Flask_SQLite_practice/tests/unit_database_tests.py
@@ -5,6 +5,7 @@ import unittest
 #from database.config import DB_PATH
 import db as db
 from models.realty_model import Realty
+from models.user_model import User
 
 class DatabaseTests(unittest.TestCase):
 
@@ -53,7 +54,7 @@ class DatabaseTests(unittest.TestCase):
             db.get_realty(1)
         self.assertIn("not found", str(context.exception))
 
-    def test_delete(self):
+    def test_delete_realty(self):
         realty = Realty(title="Koshkin dom", price=1000, city="Katcity", address="Tree street", image="some_image")
         db.create_realty(realty)
         result = db.delete_realty(realty.id)
@@ -61,7 +62,7 @@ class DatabaseTests(unittest.TestCase):
         result = db.delete_realty(10)
         self.assertFalse(result)
 
-    def test_update(self):
+    def test_update_realty(self):
         realty = Realty(title="Koshkin dom", price=1000, city="Katcity", address="Tree street", image="some_image")
         db.create_realty(realty)
         update_realty = Realty(title="Mishkin dom", price=1000, city="Katcity", address="Tree street", image="some_image", id=realty.id)
@@ -71,7 +72,42 @@ class DatabaseTests(unittest.TestCase):
         update_realty.title = "aeraherh"
         self.assertNotEqual(new_realty, update_realty)
 
+    def test_register_user(self):
+        user = User(name="Julianna", email="my@mail.com", password="hetryi459865ruhyrkjt86", reg_date="10.15.2025.11:00AM", role="user", status="active")
+        db.register_user(user)
+        print(user.id)
+        new_user = db.get_user(user.id)
+        self.assertEqual(new_user.id, user.id)
+        self.assertEqual(new_user.email, user.email)
+
+    def test_get_users(self):
+        user = User(name="Julianna", email="my@mail.com", password="hetryi459865ruhyrkjt86", reg_date="10.15.2025.11:00AM", role="user", status="active")
+        user1 = User(name="Dmitry", email="another@mail.com", password="trjtyjetyj56456756et", reg_date="10.16.2025.11:39AM", role="user", status="active")
+        db.register_user(user)
+        db.register_user(user1)
+        users = db.get_all_users()
+        self.assertEqual(len(users), 2)
+
+
+    def test_delete_user(self):
+        user = User(name="Julianna", email="my@mail.com", password="hetryi459865ruhyrkjt86", reg_date="10.15.2025.11:00AM", role="user", status="active")
+        db.register_user(user)
+        db.delete_user(user.id)
+        inactive_user = db.get_user(user.id)
+        self.assertEqual(inactive_user.status, "inactive")
+
+
+    def test_update_user(self):
+        user = User(name="Julianna", email="my@mail.com", password="hetryi459865ruhyrkjt86", reg_date="10.15.2025.11:00AM", role="user", status="active")
+        db.register_user(user)
+        update_user = User(name="Julia", email="mew@mail.com", password="newpassword4353", reg_date=user.reg_date, role=user.role, status=user.status, id=user.id)
+        db.update_user(update_user)
+        new_user = db.get_user(update_user.id)
+        self.assertEqual(new_user, update_user)
+        update_user.name = "Anna"
+        self.assertNotEqual(new_user, update_user)
 
 
 if __name__ == "__main__":
     unittest.main()
+
