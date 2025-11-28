5bda4b5314e0907547cfe72b9541e1686f02e151
Julianna Gusarova <julianna.gusasora@gmail.com>
2025-11-28 10:45:20 -0500

    database user table synchronised

--- Flask_SQLite_practice/schemas/user_model.py
+++ Flask_SQLite_practice/schemas/user_model.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, Field
-from sqlalchemy import DateTime, String, Enum, Boolean, func
+from sqlalchemy import DateTime, String, Enum, Boolean, Text, func
 from sqlalchemy.orm import Mapped, mapped_column
 from datetime import datetime, timezone
 import hashlib
@@ -52,18 +52,19 @@ class UserORM(Base):
     
     id: Mapped[int] = mapped_column(primary_key=True)
     
-    name: Mapped[str] = mapped_column(String(255), nullable=False)
-    email: Mapped[str] = mapped_column(String(120), nullable=False, unique=True)
+    name: Mapped[str] = mapped_column(Text, nullable=False)
     
-    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    email: Mapped[str] = mapped_column(Text, nullable=False, unique=True)
+    
+    password: Mapped[str] = mapped_column(Text, nullable=False)
     
     # TODO datetime DateTime(timezone=True)
-    reg_date: Mapped[str] = mapped_column(String(255), default=lambda: datetime.now(timezone.utc), server_default=func.now())
+    reg_date: Mapped[str] = mapped_column(Text, default=lambda: datetime.now(timezone.utc), server_default=func.now())
     
-    role: Mapped[str] = mapped_column(String(255), nullable=False)
+    role: Mapped[str] = mapped_column(Text, nullable=False)
     #role: Mapped[UserRole] = mapped_column(Enum(UserRole), nullable=False)
     
-    status: Mapped[str] = mapped_column(String(255), default='active')
+    status: Mapped[str] = mapped_column(Text, default='active')
     
     def __repr__(self) -> str:
         return f"<User(id={self.id}, name='{self.name}', role='{self.role}')>"
