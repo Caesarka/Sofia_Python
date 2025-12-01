d0670aa6c5ce4381428f213b570eab687a3d3979
Dmitry Gusarov <1462777+gusarov@users.noreply.github.com>
2025-12-01 16:15:19 -0500

    Introduce L3_Business_Logic

---
+++ Flask_SQLite_practice/L3_Business_Logic/realty_service.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from L2_Api_Controllers.realty_model import RealtyPatch
+import L4_Database_Access.db_sql as db_sql
+
+
+class RealtyService:
+    def __init__(self):
+        pass
+
+    def publish_realty(self, realty_id: int) -> None:
+        realty = db_sql.get_realty(realty_id, filter = 'AND is_deleted = 0')
+
+        now = datetime.now().isoformat(timespec='seconds')
+        patch_data = RealtyPatch(status=1, published_at=now)
+        db_sql.patch_realty(patch_data, realty_id)
+
---
+++ Flask_SQLite_practice/service_locator.py
@@ -0,0 +1,5 @@
+# ServiceLocator considered an anti-pattern but we need this to feel the pain and reasons to use DI frameworks later
+
+from L3_Business_Logic.realty_service import RealtyService
+
+realtyService: RealtyService = RealtyService()
