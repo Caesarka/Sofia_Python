ed313a804aed984a26bc1e3fd48f4c51398818a6
Julianna Gusarova <julianna.gusasora@gmail.com>
2026-01-20 13:34:34 -0500

    Add vue, ssr

---
+++ Flask_SQLite_practice/frontend/src/pages/Realty.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2>Realty Page</h2>
+  <p v-if="loading">Loading...</p>
+  <ul v-else>
+    <li v-for="item in realty" :key="item.id">
+      <p>{{item.id}}</p>
+      {{ item.id }}
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const realty = ref([])
+const loading = ref(true)
+
+onMounted(async () => {
+  const response = await fetch('http://127.0.0.1:5000/api/realty/1')
+  console.log('response', response)
+  
+  realty.value = await response.json()
+  console.log('realty', realty.value)
+  loading.value = false
+})
+</script>
---
+++ Flask_SQLite_practice/frontend/src/pages/SecondPage.vue
@@ -0,0 +1,60 @@
+<template>
+  <p>Hello</p>
+  <ul>
+    <li v-for="(color, index) in a" :key="index">
+      {{ color }}
+    </li>
+  </ul>
+
+  <div 
+    v-for='color in b'
+    :style="{
+      width: '100px',
+      height: '100px',
+      backgroundColor: color.hex,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }"
+    :key='color.name'
+  >
+    {{ color.name }}
+  </div>
+</template>
+
+<script setup>
+const a = [
+  'red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'
+]
+
+const b = [
+  {
+    name: 'red',
+    hex: '#FF0000'
+  },
+  {
+    name: 'orange',
+    hex: '#FFA500'
+  },
+  {
+    name: 'yellow',
+    hex: '#FFFF00'
+  },
+  {
+    name: 'green',
+    hex: '#008000'
+  },
+  {
+    name: 'blue',
+    hex: '#0000FF'
+  },
+  {
+    name: 'indigo',
+    hex: '#4B0082'
+  },
+  {
+    name: 'violet',
+    hex: '#EE82EE'
+  }
+]
+  </script>
